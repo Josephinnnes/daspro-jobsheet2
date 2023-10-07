c8e8e9fe31e48e21e8cac1b38ae0dd19959543aa
josephinnnes <joseephineez@gmail.com>
2023-10-08 00:08:08 +0700

    Jobsheet 6 modifikasi

--- Jobsheet 6/Pemilihan2Percobaan112.java
+++ Jobsheet 6/Pemilihan2Percobaan112.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
-public class Pemilihan2Percobaan112{
+public class Pemilihan2Percobaan112 {
     public static void main(String[] args) {
-        Scanner input12 = new Scanner(System.in);
-
-        System.out.println("Masukkan tahun:");
-        int tahun = input12.nextInt();
+        Scanner input12 = new Scanner (System.in);
+        int tahun;
+        System.out.println("Masukkan Tahun : ");
+        tahun=input12.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) !=0)
-            System.out.println("Tahun Kabisat");
-        } else
-        System.out.println("Bukan Tahun Kabisat");
+            if ((tahun % 100) != 0 || (tahun % 400) == 0) {
+                System.out.println("Tahun Kabisat");
+            }else{
+                System.out.println("Bukan Tahun Kabisat");
+            }
+            }else{
+                System.out.println("Bukan Tahun Kabisat");
+            }
+        }
     }
-}
